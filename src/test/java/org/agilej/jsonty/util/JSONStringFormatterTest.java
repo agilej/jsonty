27f6d2bfa5fb233af57627b2d76032d49b68b028
Donny <donnior@gmail.com>
2014-12-27 17:04:03 +0800

    testcase

---
+++ src/test/java/org/agilej/jsonty/util/JSONStringFormatterTest.java
@@ -0,0 +1,51 @@
+package org.agilej.jsonty.util;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+
+public class JSONStringFormatterTest {
+
+    @Test
+    public void test_quote_with_escape(){
+
+        assertEquals(quote(""), JSONStringFormatter.quoteWithEscape(null));
+
+        assertEquals(quote(""), JSONStringFormatter.quoteWithEscape(""));
+
+        assertEquals(quote("a"), JSONStringFormatter.quoteWithEscape("a"));
+
+        assertEquals(quote("ab"), JSONStringFormatter.quoteWithEscape("ab"));
+
+        assertEquals(quote("\\\""), JSONStringFormatter.quoteWithEscape("\""));
+
+        assertEquals(quote("\\\\"), JSONStringFormatter.quoteWithEscape("\\"));
+
+        assertEquals(quote("\\/"), JSONStringFormatter.quoteWithEscape("/"));
+
+        assertEquals(quote("\\\\"), JSONStringFormatter.quoteWithEscape("\\"));
+
+        assertEquals(quote("\\b"), JSONStringFormatter.quoteWithEscape("\b"));
+
+        assertEquals(quote("\\t"), JSONStringFormatter.quoteWithEscape("\t"));
+
+        assertEquals(quote("\\n"), JSONStringFormatter.quoteWithEscape("\n"));
+
+        assertEquals(quote("\\f"), JSONStringFormatter.quoteWithEscape("\f"));
+
+        assertEquals(quote("\\r"), JSONStringFormatter.quoteWithEscape("\r"));
+
+        assertEquals(quote(" "), JSONStringFormatter.quoteWithEscape(" "));
+
+        String s = new String(new char[]{1});
+        System.out.print(s);
+        assertEquals(quote("\\u0001"), JSONStringFormatter.quoteWithEscape(s));
+    }
+
+    private String quote(String s){
+        return "\"" + s + "\"";
+    }
+
+
+}
